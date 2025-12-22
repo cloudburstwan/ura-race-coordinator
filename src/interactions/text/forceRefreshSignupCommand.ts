8c4ac8eff6427fd5dc5596635140a9ea054bcd7d
Cloudburst <cloudburst@cloudburst.lgbt>
2025-12-22 00:24:30 +0000

    force refresh signup message command

---
+++ src/interactions/text/forceRefreshSignupCommand.ts
@@ -0,0 +1,26 @@
+﻿import TextInteraction, {TextCommandBuilder} from "../../types/TextInteraction";
+import {Message} from "discord.js";
+import DiscordClient from "../../DiscordClient";
+
+export default class ResultsCommand extends TextInteraction {
+    public info = new TextCommandBuilder()
+        .setName("forceRefreshSignup")
+        .setRegexMatch(/!forceRefreshSignup (.+)/g)
+        .addRole(process.env.RACE_STAFF_ROLE_ID);
+
+    override async execute(message: Message, regexMatch: RegExpExecArray, client: DiscordClient) {
+        let ids = regexMatch[1].split(" ");
+
+        try {
+            let race = client.services.race.races.find(race => race._id.toString("hex") == ids[0]);
+
+            if (race == undefined) throw new ReferenceError("Race not found.");
+
+            await client.services.race.updateRaceMessage(race, client, false, ids[1] == "1");
+            await message.react("✅");
+        } catch (e) {
+            await message.react("❌");
+            await message.reply(`\`${typeof e}: ${e.message}\``);
+        }
+    }
+}
